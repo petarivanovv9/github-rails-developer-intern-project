4ed3c9810952b6b02e769d6a77ec8c455c2e8aa7
Petar Ivanov <pepincho95@gmail.com>
2016-12-06 21:52:18 +0200

    Switch db from sqlite3 to postgresql

--- gryphon/db/schema.rb
+++ gryphon/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161204193419) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
     t.string   "uid"
