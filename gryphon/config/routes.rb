0dbee04f71042b6f44a0edfece21d2e79b0a1e53
Petar Ivanov <pepincho95@gmail.com>
2016-12-06 18:58:56 +0200

    Change get /sessions/destroy to post logout.

--- gryphon/config/routes.rb
+++ gryphon/config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:post, :get]
 
-  get 'sessions/destroy', as: :logout
+  post '/logout', to: 'sessions#destroy', as: 'logout'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
