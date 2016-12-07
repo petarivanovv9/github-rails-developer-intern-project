35e48e7eb1c556c3b96907130379df2c924482d1
Petar Ivanov <pepincho95@gmail.com>
2016-12-07 20:45:56 +0200

    Add create_user_from_auth_hash func in User model.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,7 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    user = User.find_or_create_by(provider: auth_hash[:provider], uid: auth_hash[:uid]) do |u|
-      u.access_token = auth_hash[:credentials][:token]
-      u.nickname     = auth_hash[:info][:nickname]
-    end
+    user = User.find_by(uid: auth_hash[:uid]) || User.create_from_auth_hash(auth_hash)
     user.update_attributes(access_token: auth_hash[:credentials][:token])
 
     session[:user_id] = user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates :access_token, presence: true, uniqueness: true
 
   validates :uid, presence: true, uniqueness: true
+
+  def self.create_from_auth_hash(hash)
+    create! do |u|
+      u.uid          = hash[:uid]
+      u.access_token = hash[:credentials][:token]
+      u.nickname     = hash[:info][:nickname]
+    end
+  end
 end
